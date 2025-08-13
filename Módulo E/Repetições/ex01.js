b1bffce67090b0c9537f21c48ec8552f09e43b91
Nicolas Samora de Souza <nicolassamora3@gmail.com>
2025-08-13 19:56:16 -0300

    Estrutura de repetição em Js

---
+++ Módulo E/Repetições/ex01.js
@@ -0,0 +1,18 @@
+//Sem repetição
+/*
+console.log('Ola mundo!')
+console.log('Ola mundo!')
+console.log('Ola mundo!')
+console.log('Ola mundo!')
+console.log('Ola mundo!')
+console.log('Ola mundo!')
+console.log('Ola mundo!')
+*/
+
+//com repetição
+//Estrutura de repetição com teste lógico no inicio
+var c = 1
+while (c <= 6) {
+    console.log('Ola mundo!')
+    c++
+}
---
+++ Módulo E/Repetições/ex02.js
@@ -0,0 +1,5 @@
+//Estrutura de repetição com teste lógico no final
+let c = 1
+do {
+    console.log('Bem vindo')
+} while (c <= 6)
