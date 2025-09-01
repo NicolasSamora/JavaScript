29cc684267ef7cd84988f548d7e00a03de022baa
Nicolas Samora de Souza <nicolassamora3@gmail.com>
2025-09-01 18:57:14 -0300

    analisador de numeros(escopo)

---
+++ Módulo F/Modelo03/script.js
@@ -0,0 +1,17 @@
+let num = document.getElementById('txtnum')
+let lis = document.getElementById('lista')
+let res = document.getElementById('res')
+let valores = []
+
+function numeros(n) {
+
+}
+
+function lista(l) {
+
+}
+
+function adicionar(n, l) {
+
+}
+
